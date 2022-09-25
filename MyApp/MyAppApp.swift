60734dac381b0364eae653ebb030cbedbac3cf62
Tomás Martins <tomas.marti@me.com>
2022-09-25 00:23:46 -0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Tomas Martins on 25/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
